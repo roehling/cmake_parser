176379efe403d87579f92e2a9e7f8a31767cc1ee
Timo Röhling <timo@gaussglocke.de>
2023-02-03 23:57:16 +0100

    Add test program

--- src/cmake_parser/__main__.py
+++ src/cmake_parser/__main__.py
@@ -13,18 +13,77 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+def prepare_args():
+    p = argparse.ArgumentParser()
+    m = p.add_mutually_exclusive_group(required=True)
+    m.add_argument(
+        "--parse-raw",
+        metavar="FILE",
+        type=argparse.FileType("r"),
+        help="use raw parser on CMake file",
+    )
+    m.add_argument(
+        "--parse",
+        metavar="FILE",
+        type=argparse.FileType("r"),
+        help="use tree parser on CMake file",
+    )
+    m.add_argument(
+        "--resolve-args",
+        metavar="ARG",
+        nargs="*",
+        help="parse and resolve CMake arguments",
+    )
+    p.add_argument(
+        "-D",
+        metavar="NAME=VALUE",
+        action="append",
+        default=[],
+        help="set CMake variable",
+    )
+    p.add_argument(
+        "--skip-comments",
+        action="store_true",
+        default=False,
+        help="filter out comments while parsing",
+    )
+    return p
+
+
 if __name__ == "__main__":
+    import argparse
     import sys
+    from pprint import pprint
 
-    if 0:
-        from pprint import pprint
+    parser = prepare_args()
+    args = parser.parse_args()
+
+    vars = {}
+    for name_value in args.D:
+        name, value = name_value.split("=", 1)
+        vars[name] = value
+
+    if args.parse_raw:
+        from .parser import parse_raw
+
+        pprint(list(parse_raw(args.parse_raw.read(), skip_comments=args.skip_comments)))
+        sys.exit(0)
+
+    if args.parse:
         from .parser import parse_tree
-        with open(sys.argv[1]) as f:
-            test_data = f.read()
-        pprint(parse_tree(test_data))
 
-    if 1:
-        from .lexer import Token
-        from .interpreter import resolve_args, Context
-        ctx = Context(var={"A": "one", "B": "red\\;violet;green;blue"})
-        print(resolve_args(ctx, [Token(kind="RAW", value="${A};${B}", span=None, line=None, column=None)]))
+        pprint(list(parse_tree(args.parse.read(), skip_comments=args.skip_comments)))
+        sys.exit(0)
+
+    if args.resolve_args:
+        from .parser import parse_raw
+        from .interpreter import Context, resolve_args
+
+        cmd = next(parse_raw(f"_({' '.join(args.resolve_args)})"))
+        ctx = Context(var=vars)
+        pprint(resolve_args(ctx, cmd.args))
+        sys.exit(0)
+
+    sys.exit(1)
